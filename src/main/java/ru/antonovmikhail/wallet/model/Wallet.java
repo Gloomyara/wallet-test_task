6e4d8d5f5ab2e09fa1a98bb576f8b269af06f9b6
Gloomyara <gloomyara@gmail.com>
2024-04-08 16:57:19 +0400

    version 1.1 added JPA @Lock

--- src/main/java/ru/antonovmikhail/wallet/model/Wallet.java
+++ src/main/java/ru/antonovmikhail/wallet/model/Wallet.java
@@ -3,14 +3,17 @@ package ru.antonovmikhail.wallet.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.Hibernate;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.math.BigDecimal;
 import java.util.Objects;
 import java.util.UUID;
 
 @Entity(name = "wallets")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @NoArgsConstructor
 @AllArgsConstructor
 public class Wallet {
@@ -21,4 +24,20 @@ public class Wallet {
     private UUID id;
     private BigDecimal amount;
     //userId, currency, etc...
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Wallet wallet = (Wallet) o;
+        return getId() != null && Objects.equals(getId(), wallet.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
--- src/main/java/ru/antonovmikhail/wallet/repository/WalletRepository.java
+++ src/main/java/ru/antonovmikhail/wallet/repository/WalletRepository.java
@@ -1,10 +1,16 @@
 package ru.antonovmikhail.wallet.repository;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 import ru.antonovmikhail.wallet.model.Wallet;
 
 import java.util.UUID;
 
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
-
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")})
+    Wallet save(Wallet wallet);
 }
